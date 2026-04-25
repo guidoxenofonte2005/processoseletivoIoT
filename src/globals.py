e26715e57c2a06ab36d68db68428e883fec476ef
Guido <guidoxenofonte@gmail.com>
2026-04-25 11:48:22 -0300

    fix(globals): fixed wrong I2C pin configuration

--- src/globals.py
+++ src/globals.py
@@ -2,7 +2,7 @@ from machine import Pin, I2C
 import dht
 
 # configuração do i2c da placa
-board_I2C = I2C(0, scl=Pin(4), sda=Pin(5))
+board_I2C = I2C(0, scl=Pin(22), sda=Pin(21))
 
 # tela
 from screen import OledScreen
